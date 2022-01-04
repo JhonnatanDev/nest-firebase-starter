5b739bbe92507f2f65d0a5d178fbe2dae73a072a
Jhonnatan Guerrero <Jhonnatan.Guerrero@foundersworkshop.com>
2022-01-03 18:06:26 -0800

    feat(firestore): Add Error handling

--- src/firestore/firestore.repository.ts
+++ src/firestore/firestore.repository.ts
@@ -1,5 +1,5 @@
 import {
-  Injectable,
+  Injectable, InternalServerErrorException, NotFoundException,
 } from '@nestjs/common';
 import { CollectionReference, DocumentReference, Query, UpdateData } from '@google-cloud/firestore';
 import { FirestoreDocument, FirestoreFilter } from './firestore.types';
@@ -15,35 +15,48 @@ export class FirestoreRepository<T> {
 
   async create(params: T, documentRef:DocumentReference = null): Promise<FirestoreDocument<T>> {
     const docRef = this.getDocumentReference(documentRef)
-    await docRef.set({id:docRef.id, ...params});
-    const documentResponse = await docRef.get();
-    return this.createFirestoreDocument(documentResponse);
+    try{
+      await docRef.set({id:docRef.id, ...params});
+      const documentResponse = await docRef.get();
+      return this.createFirestoreDocument(documentResponse);
+    } catch(error) {
+      this.handleError(error, `Failed to create document in path ${docRef.path}`)
+    }
   }
 
   async findAll(filters: FirestoreFilter[]): Promise<FirestoreDocument<T>[]> {
-    const snapshot = await this.getFirestoreSnapshot(filters);
+    try{
+      const snapshot = await this.getFirestoreSnapshot(filters);
     const data: FirestoreDocument<T>[] = [];
     snapshot.forEach(doc => {
       const document = this.createFirestoreDocument(doc);
       data.push(document)
     });
     return data;
+    } catch(error){
+      this.handleError(error, `Failed to retrieve documents at path ${this.collection.path}`)
+    }
   }
 
   async get(id: string, documentRef:DocumentReference = null): Promise<FirestoreDocument<T>> {
     const docRef = this.getDocumentReference(documentRef,id);
-    const documentResponse = await docRef.get();
-    if(!documentResponse.exists){
-      throw new Error(`Document in path ${this.collection.path} with ID ${id} not found`)
+    try{
+      const documentResponse = await docRef.get();
+      if(!documentResponse.exists){
+        throw new NotFoundException(`Document in path ${this.collection.path} with ID ${id} not found`)
+      }
+      return this.createFirestoreDocument(documentResponse);
+    } catch(error){
+      this.handleError(error, `Failed to retrieve document in path ${docRef.path} with ID ${id}`);
     }
-		return this.createFirestoreDocument(documentResponse);
   }
 
   async update(id: string, model: T, documentRef:DocumentReference = null): Promise<FirestoreDocument<T>> {
     const docRef = this.getDocumentReference(documentRef, id);
-    const documentResponse = await docRef.get();
+    try{
+      const documentResponse = await docRef.get();
     if(!documentResponse.exists){
-      throw new Error(`Document in path ${this.collection.path} with ID ${id} not found`)
+      throw new NotFoundException(`Document in path ${this.collection.path} with ID ${id} not found`)
     }
 
     Object.keys(model).forEach(
@@ -56,26 +69,37 @@ export class FirestoreRepository<T> {
     const updateModel = model as UpdateData<T>;
     await docRef.update(updateModel)
     return this.get(id);
+    } catch(error){
+      this.handleError(error, `Failed to retrieve document in path ${docRef.path} with ID ${id}`);
+    }
   }
 
   async delete(id: string, documentRef:DocumentReference = null): Promise<string>{
     const docRef = this.getDocumentReference(documentRef,id);
-    const getModelResponse = await docRef.get();
+    try{
+      const getModelResponse = await docRef.get();
 			if (!getModelResponse.exists) {
 				throw new Error(`Document in path ${this.collection.path} with ID ${id} not found`)
 			}
     await docRef.delete();
     return id;
+    } catch(error){
+      this.handleError(error, `Failed to retrieve document in path ${docRef.path} with ID ${id}`);
+    }
   }
 
   async upsert(id: string, model: T, documentRef:DocumentReference = null):Promise<FirestoreDocument<T>>{
     const docRef = this.getDocumentReference(documentRef,id);
-    const getModelResponse = await docRef.get();
+    try{
+      const getModelResponse = await docRef.get();
 			if (!getModelResponse.exists) {
 				return this.create(model,documentRef);
 			} else {
         return this.update(id, model, documentRef)
       }
+    } catch(error){
+      this.handleError(error, `Failed to retrieve document in path ${docRef.path} with ID ${id}`);
+    }
   }
 
   private getDocumentReference = (documentRef:DocumentReference, id:string = null): DocumentReference<T> => {
@@ -101,5 +125,12 @@ export class FirestoreRepository<T> {
     const documentData = snapshot.data();
 		const documentId = snapshot.id;
 		return {ref: snapshot.ref, id: documentId, data:documentData };
+  }
+
+  private handleError = (error : any, message: string) => {
+    if(error.status){
+      throw error;
+    }
+    throw new InternalServerErrorException(message)
   }
 }
